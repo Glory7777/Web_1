4a609dd7ba8222698ba09dda949b385398b332fa
Glory7777 <96227726+Glory7777@users.noreply.github.com>
2023-07-20 17:21:27 +0900

    230720_10일차
    JQuery : 다양한 요소 및 메서드
    Ajax: 시작

---
+++ HTML_CSS_JS/개인용/ex.js
@@ -0,0 +1,10 @@
+// main.js
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
