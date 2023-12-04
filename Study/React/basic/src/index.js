bf10fc4fae8c74a3e7c512251449661a3e8a768d
Glory7777 <96227726+Glory7777@users.noreply.github.com>
2023-12-04 15:16:15 +0900

    231204

--- Study/React/basic/src/index.js
+++ Study/React/basic/src/index.js
@@ -14,12 +14,16 @@ import AttendanceBook from './chapter_10/AttendanceBook';
 import SignUp from './chapter_11/SignUp';
 import Calculator from './chapter_12/Calculator';
 import ProfileCard from './chapter_13/ProfileCard';
+import DarkOrLight from './chapter_14/DarkOrLight';
+import Blocks from './chapter_15/Blocks';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 setInterval(()=> {
 root.render(
   <React.StrictMode>
+    <Blocks/>
+    <DarkOrLight/>
     <Library/>
     <Clock/>
     <CommentList />
