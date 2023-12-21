c1a6a541479b71d1c7ddbef0f37c57f815d59b0f
Glory7777 <96227726+Glory7777@users.noreply.github.com>
2023-12-21 17:12:18 +0900

    추가 수정
    무비웹. 라우터 수정 보완
    리덕스 리액트버젼 새로 파일생성

--- Study/React/movie_web/src/App.js
+++ Study/React/movie_web/src/App.js
@@ -1,20 +1,18 @@
-import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react";
-import Home from "./routes/Home";
-import Detail from "./routes/Detail";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./routes/Home"
+import Detail from "./routes/Detail"
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />
+        {/* 추가적인 라우트들을 정의하세요 */}
+      </Routes>
     </Router>
-  )
-  };
+  );
+}
+
 export default App;
