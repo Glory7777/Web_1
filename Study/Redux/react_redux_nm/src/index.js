2c19799444a98bfd7b9787f8e35ff67972f23348
Glory7777 <96227726+Glory7777@users.noreply.github.com>
2023-12-22 16:22:00 +0900

    추가설명

--- Study/Redux/react_redux_nm/src/index.js
+++ Study/Redux/react_redux_nm/src/index.js
@@ -9,4 +9,5 @@ root.render(
     <Provider store={store}>
 <App />
 </Provider>,
+//<App /> 컴포넌트가 <Provider> 컴포넌트로 감싸져 있으며, store라는 Redux 스토어가 <Provider> 컴포넌트에 전달되어 전역 상태를 관리할 수 있도록 설정된것
 );
--- Study/Redux/react_redux_nm/src/routes/Detail.js
+++ Study/Redux/react_redux_nm/src/routes/Detail.js
@@ -11,6 +11,16 @@ const toDo = toDos.find((toDo) => toDo.id === parseInt(myId));
 return (
 <>
 <h1>{toDo?.text}</h1>
+
+{/* toDo?.text는 옵셔널 체이닝(optional chaining)이라는 JavaScript의 새로운 문법 중 하나
+    객체의 중첩된 속성에 접근할 때 해당 속성이 존재하지 않을 경우 오류를 방지하기 위해 도입
+
+    toDo가 존재하면:
+    toDo 객체에 text 속성이 존재하는지 확인합니다.
+    만약 toDo.text가 존재한다면, 그 값을 반환합니다.
+    toDo가 undefined 또는 null이면:
+    이어지는 속성(text)에는 접근하지 않고 바로 undefined를 반환
+*/}
 <h5>Created at: {toDo?.id}</h5>
 </>
 );
