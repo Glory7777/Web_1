2c570ddad8a5beb5f6506b2a5159fe3f2c137f7e
Glory7777 <96227726+Glory7777@users.noreply.github.com>
2023-12-27 18:00:07 +0900

    231227_리액트_전체구조파악

--- Study/React/official_doc/src/main.jsx
+++ Study/React/official_doc/src/main.jsx
@@ -2,11 +2,13 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
+import Game from "./game";
 import App from "./app";
-
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
+    <Game/>
+    <br/>
     <App />
   </StrictMode>
 );
